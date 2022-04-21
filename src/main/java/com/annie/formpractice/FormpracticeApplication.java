6e29035107ebce9c1e708e3e785eba15e05ffdfc
Annie Jung <younjujung27@gmail.com>
2022-04-20 22:21:37 -0700

    first commit

---
+++ src/main/java/com/annie/formpractice/FormpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.annie.formpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormpracticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/annie/formpractice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.annie.formpractice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FormpracticeApplication.class);
+	}
+
+}
